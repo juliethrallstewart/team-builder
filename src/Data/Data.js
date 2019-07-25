afefdb5768107749e545c0a079ef939b086ab795
Julie Thrall <juliethrall10@gmail.com>
2019-07-24 18:32:32 -0700

    completed steps 1 & 2

---
+++ src/Data/Data.js
@@ -0,0 +1,13 @@
+export default [
+	{
+		data : [
+			{
+				type     : 'member',
+				id       : '0',
+				name     : 'JSON:API paints my bikeshed!',
+				password : 'The shortest article. Ever.',
+				email    : '2015-05-22T14:56:29.000Z'
+			}
+		]
+	}
+];
